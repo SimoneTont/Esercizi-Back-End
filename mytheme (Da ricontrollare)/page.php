f5686a70a1a2566b069582aade309fc9f31f4e6b
Simone Tonti <simone.tonti98@gmail.com>
2024-02-19 09:14:27 +0100

    estratta cartella mytheme

---
+++ mytheme (Da ricontrollare)/page.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col-8">
+            <h1 class="text-center">Page: <?php bloginfo('name'); ?></h1>
+
+            <?php
+            if (is_user_logged_in()) {
+                echo 'Welcome, ' . wp_get_current_user()->display_name . '!';
+            } else {
+                echo 'Welcome, visitor!';
+            }
+            ?>
+
+        </div>
+        <div class="col-4">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
---
+++ mytheme (Da ricontrollare)/search.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col-8">
+            <h1 class="text-center">Search: <?php bloginfo('name'); ?></h1>
+        </div>
+        <div class="col-4">
+            <?php get_sidebar(); ?>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
